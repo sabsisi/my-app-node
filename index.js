b19a1ba110c04da05035d17401078e4b98c90a66
Sabsisi <goucemsabrina43@gmail.com>
2025-08-10 16:41:58 +0200

    Mon premier déploiement

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('Hello');
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Permet de servir tous les fichiers du dossier courant (HTML, CSS, JS...)
+app.use(express.static(path.join(__dirname)));
+
+// Route d'accueil
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'passeword.html'));
+});
+
+const PORT = process.env.PORT || 3000;  // Utilise le port du cloud ou 3000 en local
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`✅ Serveur prêt sur le port ${PORT}`);
+});
